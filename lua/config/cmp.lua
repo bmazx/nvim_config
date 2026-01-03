0e2ce9f21967e1133cf2d2ffe28342f1eed5939e
Brian Ma <105564191+bmazx@users.noreply.github.com>
2026-01-02 22:17:37 -0500

    remove cmp for searching with /

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -82,12 +82,12 @@ cmp.setup({
 equire("cmp_git").setup() ]]--
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
+-- cmp.setup.cmdline({ '/', '?' }, {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 -- cmp.setup.cmdline(':', {
